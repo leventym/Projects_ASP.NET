c408ade8c391e3c37a47f8653622966ee1850f8b
Levent Yildirim <leventym86@hotmail.com>
2022-04-04 00:48:14 +0200

    new project

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+        //Relationships
+        public List<Actor_Movie> Actors_Movies { get; set; }
+    }
+}
