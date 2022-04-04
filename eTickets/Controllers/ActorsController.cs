7986cf6f72fc0581d7e18f888aada3e9cec29b7d
Levent Yildirim <leventym86@hotmail.com>
2022-04-04 02:05:33 +0200

    development

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View(data);
+        }
+    }
+}
