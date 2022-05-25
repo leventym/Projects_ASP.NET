5876ea20ba7956ad3c4ae5d3409060392868e1a1
Levent <leventym86@hotmail.com>
2022-05-25 15:42:03 +0200

    added EntityBaseRepository and interface for it

---
+++ eTicket/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eTicket.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        public Task AddAsync(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(int id, T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ eTicket/Data/Base/IEntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace eTicket.Data.Base
+{
+    public interface IEntityBase
+    {
+        int Id { get; set; }
+
+    }
+}
