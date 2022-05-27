448f1878aeec43c2403f57be265defed89e362e2
Levent <leventym86@hotmail.com>
2022-05-27 03:55:07 +0200

    updated search string

--- eTicket/Controllers/MoviesController.cs
+++ eTicket/Controllers/MoviesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -35,9 +36,13 @@ namespace eTicket.Controllers
 
             if (!string.IsNullOrEmpty(searchString))
             {
-                var filteredResult = allMovies.Where(n => n.Name.Contains(searchString) || n.Description.Contains(searchString)).ToList();
 
-                return View("Index", filteredResult);
+                var filteredResultNew = allMovies.Where(n => string.Equals(n.Name, searchString,
+                    StringComparison.CurrentCultureIgnoreCase) || string.Equals(n.Description, searchString,
+                    StringComparison.CurrentCultureIgnoreCase)).ToList();
+
+
+                return View("Index", filteredResultNew);
             }
 
             return View("Index", allMovies);
