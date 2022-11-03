da9dfeb9925211e5972d301aeddb8b4a9bd0e1e6
Levent Yildirim <leventyildirim@icloud.com>
2022-11-03 16:26:24 +0100

    created new db

---
+++ AbbyWeb/Model/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AbbyWeb.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
+
