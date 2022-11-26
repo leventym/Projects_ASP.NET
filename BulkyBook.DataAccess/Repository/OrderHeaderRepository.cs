05d1b29686bdd89eccdd6589df2f280a8518bd08
Levent Yildirim <leventyildirim@icloud.com>
2022-11-26 15:25:10 +0100

    order status

--- BulkyBook.DataAccess/Repository/OrderHeaderRepository.cs
+++ BulkyBook.DataAccess/Repository/OrderHeaderRepository.cs
@@ -36,7 +36,7 @@ namespace BulkyBook.DataAccess.Repository
         public void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId)
         {
             var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
-
+            orderFromDb.PaymentDate = DateTime.Now;
             orderFromDb.SessionId = sessionId;
             orderFromDb.PaymentIntentId = paymentIntentId;
         }
